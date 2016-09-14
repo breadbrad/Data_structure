4d4a8fa0b3bd7fc7acdc3931f7fdd6e18e608ef3
breadbrad <yimdonghoon@gmail.com>
2016-09-14 03:00:26 -0500

    Add Red-Black tree

---
+++ Self_balancing_search_tree/RedBlackTree.java
@@ -0,0 +1,12 @@
+/**
+Invariants ;
+1; A node is either red or black
+2; The root is always black
+3; A red node always has black children
+( A null reference is considered to refer to a black node)
+4; The number of black nodes in any path from the root to a leaf is the same
+
+Insertion into a Red-Black Tree
+- When a leaf is found, the new item is inserted (with the color red [invariant 4])
+
+*/
